c3668e774e3ca048693c74bb92642f58fe612415
Arihant Jain <ajain38@rocketmail.com>
2021-04-16 20:48:43 +0530

    handle missing values based on correlation matrix
    Correlation-based features like age and fare.
    Use the whole name.
    Train: 0.79902
    Test:  0.79163

--- main.py
+++ main.py
@@ -14,37 +14,65 @@ train = train.drop(['Survived'],1)
 test = pd.read_csv(os.path.join(root_path, 'test.csv'))
 # print('Test dataset:\n', test.head())
 
-# sample_submission = pd.read_csv(os.path.join(root_path, 'sample_submission.csv'))
 
 dataset = pd.concat([train, test], axis = 0, ignore_index = True)
 train_len = len(train)
 
-dataset = dataset.drop(['PassengerId'],1)
+dataset = dataset.drop(['PassengerId'], 1)
+print("Correlation Matrix:\n", dataset.corr())
 print('Whole Dataset:\n', dataset.head())
 # print('Types of columns:\n', dataset.dtypes)
 print('Description of dataset:\n', dataset.describe(include='all'))
 
-
-categorical_feature_columns = dataset.select_dtypes(exclude=['float64']).columns
-# print('Categorical features:', categorical_feature_columns)
-
-label = LabelEncoder()
-for column in categorical_feature_columns:
-    # label.fit(dataset[column].astype(str))
-    # dataset[column] = label.transform(dataset[column])
-    dataset[column] = label.fit_transform(dataset[column].astype(str))
-
-# print('Whole Dataset:\n', dataset.head())
-# print('Description of dataset:\n', dataset.describe(include='all'))
-# print('Types of columns:\n', dataset.dtypes)
+""" Handling Missing Data"""
 
 # print('Missing values per columns in dataset')
 # for col in dataset.columns:
 #     temp_col = dataset[col].isnull().sum()
 #     print(f'{col}: {temp_col}')
 
-dataset = dataset.drop(['Age', 'Fare'], 1)
-print('Whole Dataset:\n', dataset.head())
+map_age_pclass = dataset[['Age', 'Pclass']].dropna().groupby('Pclass').mean().to_dict()
+dataset['Age'] = dataset['Age'].mask(dataset['Age'].isna(), dataset['Pclass'].map(map_age_pclass['Age']))
+
+dataset["Cabin"] = dataset["Cabin"].fillna("Nan")
+# print('Cabin Whole Dataset:\n', dataset.head())
+
+dataset["Ticket"] = dataset["Ticket"].fillna("Nan")
+
+map_age_pclass = dataset[['Fare', 'Pclass']].dropna().groupby('Pclass').mean().to_dict()
+dataset['Fare'] = dataset['Fare'].mask(dataset['Fare'].isna(), dataset['Pclass'].map(map_age_pclass['Fare']))
+
+dataset["Embarked"] = dataset["Embarked"].fillna("Nan")
+
+# print('Whole Dataset:\n', dataset.head())
+
+# print('Missing values per columns in dataset')
+# for col in dataset.columns:
+#     temp_col = dataset[col].isnull().sum()
+#     print(f'{col}: {temp_col}')
+
+# print(len(dataset.Ticket.unique()))
+
+# print(dataset['Age'].isna().sum())
+
+categorical_feature_columns = dataset.select_dtypes(exclude=['float64']).columns
+# # print('Categorical features:', categorical_feature_columns)
+
+label = LabelEncoder()
+for column in categorical_feature_columns:
+    dataset[column] = label.fit_transform(dataset[column].astype(str))
+
+# print('Whole Dataset:\n', dataset.head())
+# # print('Description of dataset:\n', dataset.describe(include='all'))
+# # print('Types of columns:\n', dataset.dtypes)
+
+# # print('Missing values per columns in dataset')
+# # for col in dataset.columns:
+# #     temp_col = dataset[col].isnull().sum()
+# #     print(f'{col}: {temp_col}')
+
+# dataset = dataset.drop(['Age', 'Fare'], 1)
+# print('Whole Dataset:\n', dataset.head())
 
 
 
@@ -69,3 +97,5 @@ def submit(filename):
     submission = pd.DataFrame({'PassengerId': test['PassengerId'],'Survived': ypred})
     # print("Submission:\n", submission.head())
     submission.to_csv(filename, index=False)
+
+# submit("submission2-corr_based_missing_val.csv")
